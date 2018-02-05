e7a3624a3df91adf7cbca410b1585e2c090a91ef
Askil <olsena@uni.coventry.ac.uk>
2018-02-05 12:55:50 +0100

    First commit

--- lab2.10a.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-	int i=0;
-	if (i == 0){
-	i++;
- }
-return 0;
-}
--- lab2.10b.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-	int i;
-	for (int i=0; i<10; i++){
-	i++;
-}
-return 0;
-}
