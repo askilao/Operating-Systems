918e924abee9dc827c8629926a868f9cc1ba157f
askilao <askil_a_o@live.no>
2018-04-18 07:37:14 +0100

    Edited from Handles to Id in chromeThreadCount

--- oblig3/chromeThreadCount.ps1
+++ oblig3/chromeThreadCount.ps1
@@ -1,1 +1,1 @@
-foreach($handle in (Get-Process chrome).Handles  ){ Write-Output "chrome $handle $((Get-Process chrome | Where-Object {$_.Handles -eq $handle } | Select-Object -ExpandProperty Threads).count) "}
+foreach($id in (Get-Process chrome).id  ){ Write-Output "chrome $id $((Get-Process chrome | Where-Object {$_.id -eq $id } | Select-Object -ExpandProperty Threads).count) "}
